846e2fb0992406c5ded2a6938c3332c966323849
company <nicolas.lshuang@gmail.com>
2015-03-13 17:57:39 +0800

    add osom

---
+++ components/osom-tables/osom-tables.js
@@ -0,0 +1,11 @@
+Polymer('osom-table-container', {
+  source : '',
+});
+Polymer('osom-table');
+Polymer('osom-table-body');
+Polymer('osom-header');
+Polymer('osom-header-sort', {
+  name : ''
+});
+Polymer('osom-filters');
+Polymer('osom-text-filter');
